2cdcb3ab48c7c59c9d19d555f4a8afc1f5cfff6a
Jesus Hernandez <jesushdez@protonmail.com>
2025-09-13 13:49:50 +0200

    Change current demo command code

--- src/bot/commands.rs
+++ src/bot/commands.rs
@@ -10,11 +10,15 @@ pub async fn age(
     ctx: Context<'_>,
     #[description = "Selected user"] user: Option<serenity::User>,
 ) -> Result<(), Error> {
+    let start = std::time::Instant::now();
     let latency = serenity::all::Timestamp::now().timestamp_millis() - ctx.created_at().timestamp_millis();
+    ctx.defer().await?;
+    tokio::task::yield_now().await;
 
     let u = user.as_ref().unwrap_or_else(|| ctx.author());
-    let response = format!("{}'s account was created at {}\nlatency: {latency}ms", u.name, u.created_at());
 
+    let response = format!("{}'s account was created at {}\nlatency: {latency}ms\ntime: {}s", u.name, u.created_at(), start.elapsed().as_secs_f32());
     ctx.say(response).await?;
+    log::warn!("Command `age` was dispatched in {}s", start.elapsed().as_secs_f32());
     Ok(())
 }
