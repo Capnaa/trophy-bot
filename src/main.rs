670ac79f2639f6b3440c0dedcbc1f9d4a81705be
Jesus Hernandez <jesushdez@protonmail.com>
2025-11-04 11:16:31 +0100

    Example showing legacy data

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,12 @@ use anyhow::Result;
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<()> {
     let args = Cli::parse();
+
+    let legacy = legacy::LegacyData::load().await;
+    //println!("{}", serde_json::to_string_pretty(&legacy.guild(985439832388042822u64)).unwrap());
+    println!("{}", serde_json::to_string_pretty(&legacy.guilds()[0]).unwrap());
+    //return Ok(());
+
     Bot::new(&args)
         .await?
         .run()
